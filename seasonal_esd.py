8a319413e895cec2552848a1d0647f7abde81d94
Nacho <ignacio.navarro14@imperial.ac.uk>
2017-10-09 17:37:41 +0100

    Add calculations of critical value and test statistic

--- seasonal_esd.py
+++ seasonal_esd.py
@@ -1,4 +1,21 @@
+import scipy.stats as stats
 import numpy as np
 
+def calculate_test_statistic(ts):
+    zscores = stats.zscore(ts)
+    max_idx = np.argmax(zscores)
+    return max_idx
+
+def calculate_critical_value(ts, alpha):
+    size   = len(ts)
+    t_dist = stats.t.sf(alpha / (2 * size), size - 2)
+    
+    numerator   = (size - 1) * t_dist
+    denominator = np.sqrt(size ** 2 - size * 2 + size * t ** 2)
+
+
 def seasonal_esd(ts, max_anomalies=10, alpha=0.05):
-	pass
+    pass
+
+
+def esd(ts, max_anomalies=10, alpha=0.05):
